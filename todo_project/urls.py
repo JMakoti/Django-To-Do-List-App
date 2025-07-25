f0d6caf3b108cf33b1e89f5008ef8f0c8fd9eb2d
JMakoti <josephmwamuye11@gmail.com>
2025-07-25 15:28:18 +0300

    update todo home url

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todos/',include('todo_app.urls')),
+    path('',include('todo_app.urls')),
 ]
